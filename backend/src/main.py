4a1f324f84cffc1b689117b8c30b8628637d6880
张锦华 <colin>
2026-03-21 13:12:34 +0800

    refactor: 删除 toolsets 和 ai_tool_categories 管理路由
    - 删除 backend/src/interfaces/routers/admin/toolsets.py
    - 删除 backend/src/interfaces/routers/admin/ai_tool_categories.py
    - 更新 main.py 删除对应的导入和路由注册
    
    这些路由已被 ai_tools.py 中的统一管理端点替代。

--- backend/src/main.py
+++ backend/src/main.py
@@ -25,8 +25,6 @@ from src.interfaces.routers import users as new_users_router
 from src.interfaces.routers.admin import tools as new_admin_tools_router
 from src.interfaces.routers.admin import navigation as new_admin_navigation_router
 from src.interfaces.routers.admin import navigation_categories as new_admin_navigation_categories_router
-from src.interfaces.routers.admin import toolsets as new_admin_toolsets_router
-from src.interfaces.routers.admin import ai_tool_categories as new_admin_ai_tool_categories_router
 from src.interfaces.routers.admin import ai_tools as new_admin_ai_tools_router
 from src.interfaces.routers.admin import model_providers as new_admin_model_providers_router
 from src.interfaces.routers import works as new_works_router
@@ -83,8 +81,6 @@ app.include_router(new_users_router.router)
 app.include_router(new_admin_tools_router.router)
 app.include_router(new_admin_navigation_router.router)
 app.include_router(new_admin_navigation_categories_router.router)
-app.include_router(new_admin_toolsets_router.router)
-app.include_router(new_admin_ai_tool_categories_router.router)
 app.include_router(new_admin_ai_tools_router.router)
 app.include_router(new_admin_model_providers_router.router)
 
